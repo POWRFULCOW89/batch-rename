aa5f6e1aaa7cab9ec749eece982f058f2fea7350
Diego Melo <diego_14melo@hotmail.com>
2021-09-02 10:06:30 -0500

    Tests and docs

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
 from os import listdir, rename
 from os.path import isfile, splitext, join, isdir
 from argparse import ArgumentParser
+from sys import stderr
 
+# Initializing the CLI tool with four parameters
 parser = ArgumentParser(description="Batch rename files, stripping a given substring")
-
 parser.add_argument("path", metavar="path", type=str, default="./", nargs="?", help="Path to directory with files to rename. Defaults to current working directory.")
 parser.add_argument("substring", metavar="substring", type=str, help="Substring to strip or replace out of every file in path.")
 parser.add_argument("new_string", metavar="new_string", type=str, default="", nargs="?", help="String to replace for every matching substring.")
 parser.add_argument('-n', action='store_true', help="Rename all files to substring and number them accordingly.")
 
-args = parser.parse_args()
-path = args.path
-substring = args.substring
-numbers = args.n
-new_string = args.new_string
+def scanDir(path):
+    """Takes a path and returns a list of all existing files.
 
-def scanDir():
-    if isdir(path):
+    Args:
+        path (str): A resolvable path to search for files in a non-recursive manner.
+
+    Returns:
+        list: Valid files in directory.
+    """
+    if isdir(path): # Making sure it's a valid directory
         files = [f for f in listdir(path) if isfile(join(path, f))] # Filter out files only
         if len(files) > 0:
             return files # Return what was found
         else: 
-            print("Directory is empty")
+            print("Directory is empty", file=stderr) # Outputting errors to the standard error stream.
     else:
-        print(path, "is not a directory or does not exist.")
+        print(path, "is not a directory or does not exist.", file=stderr)
 
 
-def renameFiles(files):
-    count = 0
+def renameFiles(path, files, substring, new_string, numbers):
+    """Main renaming function. All ocurrences of substring will be replaced with new_string in a given path 
+    that has at least one file. If numbers is set to true, all files will be renamed to substring and will be appended a 
+    number in ascending order.
+
+    Args:
+        path (str): A resolvable path to look for files in.
+        files (list(str)): A list containing valid files in the form of strings.
+        substring (str): Substring that will be replaced.
+        new_string (str): String to replace substring with.
+        numbers (bool): If set to true, all files will be renamed to substring and appended a number in ascending order.
+
+    Returns:
+        int: Count of files renamed.
+    """
+    count = 0 # Final count to return of successful renames
 
     for file in files:
 
-        name, ext = splitext(file)
-
+        name, ext = splitext(file) # Destructuring the name and extension of each file
         newName = name
 
         if numbers: 
-            newName = substring + str(count + 1)
+            newName = substring + str(count + 1) # Appending a number if numbers is true
 
         elif name.find(substring) != -1:
-            newName = name.replace(substring, new_string)
+            newName = name.replace(substring, new_string) # Renaming
         
         if numbers or name.find(substring) != -1: # if the numbers flag is present or a file can be renamed, then
             count += 1
@@ -48,14 +64,22 @@ def renameFiles(files):
 
     if count > 0: 
         print("Successfully renamed {0} files.".format(count))
+        return count
     else: 
-        print("No files renamed.")
+        print("No files renamed.", file=stderr)
 
 if __name__ == '__main__':
     try:
-        files = scanDir()
+        # Making sure the CLI tool only works when run independently, allowing for modularization and testing
+        args = parser.parse_args()
+        path = args.path
+        substring = args.substring
+        numbers = args.n
+        new_string = args.new_string
+
+        files = scanDir(path)
         if files:
-            renameFiles(files)
+            renameFiles(path, files, substring, new_string, numbers)
 
     except Exception as e:
         print(e)
---
+++ test.py
@@ -0,0 +1,86 @@
+from main import scanDir, renameFiles
+import unittest
+from os import mkdir, rmdir
+from os.path import exists
+from shutil import rmtree
+
+class TestBatchRename(unittest.TestCase):
+
+    def test_a_createFiles(self):
+        if not exists("tests"): mkdir("tests")
+        for _ in range(5):
+            with open('tests/' + "DummyFileNumber" + str(_) + '.txt', 'w') as f:
+                f.write(str(_))
+        self.assertTrue(exists("tests"))
+        
+
+    def test_b_scanFullDir(self):
+        files = scanDir("./tests")
+        self.assertEqual(files, ['DummyFileNumber0.txt',
+            'DummyFileNumber1.txt',
+            'DummyFileNumber2.txt',
+            'DummyFileNumber3.txt',
+            'DummyFileNumber4.txt'])
+
+    def test_c_scanEmptyDir(self):
+        if not exists("test_empty"): mkdir("test_empty")
+        if not exists("test_empty/folder"): mkdir("test_empty/folder")
+        files = scanDir("test_empty")
+        self.assertIsNone(files)
+        rmdir("test_empty/folder")
+        rmdir("test_empty")
+        
+
+    def test_d_scanFakeDir(self):
+        files = scanDir("money")
+        self.assertIsNone(files)
+        
+    def test_e_scanFile(self):
+        files = scanDir("/main.py")
+        self.assertIsNone(files)
+
+    # renameFiles(path, files, substring, new_string, numbers)
+    def test_f_rename(self):
+        files = scanDir("./tests")
+        count = renameFiles("./tests", files, "Dummy", "", False )
+        files = scanDir("./tests")
+        self.assertEqual(files, 
+        [   'FileNumber0.txt',
+            'FileNumber1.txt',
+            'FileNumber2.txt',
+            'FileNumber3.txt',
+            'FileNumber4.txt'])
+        self.assertEqual(count, 5)
+
+    def test_g_replace(self):
+        files = scanDir("./tests")
+        count = renameFiles("./tests", files, "File", "Archive", False )
+        files = scanDir("./tests")
+        self.assertEqual(count, 5)
+        self.assertEqual(files, 
+        [   'ArchiveNumber0.txt',
+            'ArchiveNumber1.txt',
+            'ArchiveNumber2.txt',
+            'ArchiveNumber3.txt',
+            'ArchiveNumber4.txt'])
+
+    def test_h_rename_numerically(self):
+        files = scanDir("./tests")
+        count = renameFiles("./tests", files, "File_case_number_", "", True )
+        files = scanDir("./tests")
+        files.sort()
+        self.assertEqual(count, 5)
+        self.assertListEqual(files, 
+        [   'File_case_number_1.txt',
+            'File_case_number_2.txt',
+            'File_case_number_3.txt',
+            'File_case_number_4.txt',
+            'File_case_number_5.txt'])
+        
+    def test_i_deleteFiles(self):
+        rmtree("tests")
+        # help(scanDir)
+        # help(renameFiles)
+
+if __name__ == '__main__':
+    unittest.main()
